7d4025b93beb456a00df093f28562907996098a0
Kim Love <kimberlymlove15@gmail.com>
2023-10-05 13:31:15 -0400

    Code along complete

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -11,5 +11,18 @@ class GetPrograms:
     return response.content
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_schools(self):
+    programs = json.loads(self.get_programs())
+
+    agencies = [program["agency"] for program in json.loads(self.get_programs())]
+    return agencies
+
+# Makes creates an instance of GetPrograms and then calls the instance method all in one like
+programs = GetPrograms().program_schools()
+
+for school in set(programs):
+  print(school)
+
+# Another method to go about it
+# programs = GetPrograms() # First create an instance of GetPrograms
+# print(programs.get_programs()) # Calling the instance method on the object
